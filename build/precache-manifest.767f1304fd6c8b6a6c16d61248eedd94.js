a43d89915e26fb903c81cccbf667ceb17a348b79
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-05-03 16:00:44 +0200

    Deploy to Heroku with serving Static Build Folder copied from the Client

---
+++ build/precache-manifest.767f1304fd6c8b6a6c16d61248eedd94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2751b5635ff5557cc10f993287b2fab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03f514fd571f52638599",
+    "url": "/static/css/main.9b733ba6.chunk.css"
+  },
+  {
+    "revision": "d1d0ef480b2a39f1d881",
+    "url": "/static/js/2.cf8a4139.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf8a4139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f514fd571f52638599",
+    "url": "/static/js/main.6c470e82.chunk.js"
+  },
+  {
+    "revision": "1eec29fcfe38d9538469",
+    "url": "/static/js/runtime-main.c6c6614c.js"
+  }
+]);
--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 
 dotenv.config({ path: './config/config.env' });
 
