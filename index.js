f58ab1dadca129bc50dd877af9ee75dc96bda3b8
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2020-05-09 14:37:53 +0200

    Exercises 3.15.-3.18. & Created a seeder.js file to reset initial data if necessary

--- index.js
+++ index.js
@@ -10,29 +10,6 @@ const morgan = require('morgan');
 const cors = require('cors');
 const Person = require('./models/Person');
 
-// let persons = [
-//   {
-//     name: 'Arto Hellas',
-//     number: '040-123456',
-//     id: 1,
-//   },
-//   {
-//     name: 'Ada Lovelace',
-//     number: '39-44-5323523',
-//     id: 2,
-//   },
-//   {
-//     name: 'Dan Abramov',
-//     number: '12-43-234345',
-//     id: 3,
-//   },
-//   {
-//     name: 'Mary Poppendieck',
-//     number: '39-23-6423122',
-//     id: 4,
-//   },
-// ];
-
 const app = express();
 connectDB();
 
@@ -54,19 +31,26 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // ROUTES =========================================
-app.get('/api/persons', async (req, res) => {
+app.get('/info', async (req, res, next) => {
+  try {
+    const persons = await Person.find({});
+
+    res.send(`Your phonebook has ${persons.length} entries`);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get('/api/persons', async (req, res, next) => {
   try {
     const persons = await Person.find({});
     res.send(persons);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      error: 'Something went wrong',
-    });
+    next(error);
   }
 });
 
-app.get('/api/persons/:id', async (req, res) => {
+app.get('/api/persons/:id', async (req, res, next) => {
   try {
     const person = await Person.findById(req.params.id);
 
@@ -74,14 +58,11 @@ app.get('/api/persons/:id', async (req, res) => {
 
     res.send(person);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      error: 'Something went wrong',
-    });
+    next(error);
   }
 });
 
-app.post('/api/persons', async (req, res) => {
+app.post('/api/persons', async (req, res, next) => {
   try {
     const { name, number } = req.body;
 
@@ -94,24 +75,52 @@ app.post('/api/persons', async (req, res) => {
 
     res.send(person);
   } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      error: 'Something went wrong',
-    });
+    next(error);
   }
 });
 
-app.delete('/api/persons/:id', async (req, res) => {
+app.put('/api/persons/:id', async (req, res, next) => {
+  try {
+    let person = await Person.findById(req.params.id);
+
+    if (!person) return res.status(404).end();
+
+    person = await Person.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.send(person);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete('/api/persons/:id', async (req, res, next) => {
   try {
     await Person.findByIdAndDelete(req.params.id);
     res.status(204).end();
   } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      error: 'Something went wrong',
-    });
+    next(error);
   }
 });
+
+// Error handler middleware
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({
+    error: 'unknown endpoint',
+  });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err.message);
+
+  if (err.name === 'CastError')
+    return res.status(400).send({ error: 'malformed id' });
+};
+
+app.use(errorHandler);
 
 // SERVER =======================================
 const PORT = process.env.PORT || 3001;
---
+++ seeder.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const colors = require('colors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config({ path: './config/config.env' });
+
+// Load model
+const Person = require('./models/Person');
+
+// Connect to db
+connectDB();
+
+// Read JSON file
+const persons = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/persons.json`, 'utf-8')
+);
+
+// Operations
+const importData = async () => {
+  try {
+    await Person.create(persons);
+
+    console.log('Data imported...'.green.inverse);
+
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Person.deleteMany();
+
+    console.log('Data deleted...'.red.inverse);
+
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// we need to run these commands in the console -> "node seeder -i" || "node seeder -d"
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
